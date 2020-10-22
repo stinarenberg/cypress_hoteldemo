47d55ab6a599cbffe09e51b05cfe1109f82a7be9
Stina Renberg <stinarenberg1@gmail.com>
2020-10-22 09:49:59 +0200

    created a .gitignore and test project

---
+++ frontend-tests/cypress/integration/regression-testsuite.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+//This is a testsuite
+describe ('Regression test suite', function() {
+
+    //This is  a test case
+    it('Perform valid login', function(){
+        cy.visit('http://localhost:3000')
+
+    })
+
+
+
+})
